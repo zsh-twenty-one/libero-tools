d758e57f31e12b0317d78ce0bbbf4d47d3ce0f2c
zhaoshihao <1962710675@qq.com>
2021-11-20 22:36:24 +0800

    feat(add function)：commit "randomColor" tool function

--- src/main.js
+++ src/main.js
@@ -1,2 +1,3 @@
 export { equals } from "./tools/equals/index";
 export { onClickSide } from "./tools/clickOutSide/index";
+export { randomColor } from "./tools/randomColor/index";
--- src/tools/clickOutSide/index.js
+++ src/tools/clickOutSide/index.js
@@ -1,12 +1,11 @@
 /**
- * 
- * @param {容器节点} parentNode 
- * @param {判定节点} childrenNode 
- * @param {返回函数，传入判定结果} callback 
+ *
+ * @param {容器节点} parentNode
+ * @param {判定节点} childrenNode
+ * @param {返回函数，传入判定结果} callback
  */
 export const onClickSide = (parentNode, childrenNode, callback) => {
   if (parentNode?.nodeName && childrenNode?.nodeName) {
     callback(parentNode.contains(childrenNode));
   }
-  throw new Error("请输入正确的节点！");
 };
